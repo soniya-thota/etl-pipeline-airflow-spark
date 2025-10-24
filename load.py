42fefbc0d7ac2869d69f0bb2c6bd0906b02b882f
Soniya <126555086+soniya-thota@users.noreply.github.com>
2025-10-24 18:43:30 -0400

    Delete load.py

--- load.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-def load_to_s3(df, bucket_name, key):
-    s3 = boto3.client('s3')
-    df.to_csv('/tmp/final_output.csv', index=False)
-    s3.upload_file('/tmp/final_output.csv', bucket_name, key)
-    print(f"Data loaded to s3://{bucket_name}/{key}")
