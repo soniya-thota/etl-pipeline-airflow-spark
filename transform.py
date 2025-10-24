708f9e3b5bde7e924c0a738b9dfd198fef6180a6
Soniya <126555086+soniya-thota@users.noreply.github.com>
2025-10-24 18:43:44 -0400

    Delete transform.py

--- transform.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def transform_data(df):
-    
-    df = df.dropna()
-    df['total_price'] = df['quantity'] * df['unit_price']
-    print("Transformation complete.")
-    return df
